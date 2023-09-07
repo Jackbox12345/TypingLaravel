f6619310331b62eaabae96fc50c8e1f738a9dbb4
Jackbox12345 <lapitanchristian258@gmail.com>
2023-09-08 05:50:25 +0800

    Model issue

---
+++ app/Models/typingTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class typingTest extends Model
+{
+    use HasFactory;
+    protected $table = 'typingTest';
+    protected $fillable =['user_id','wpm','mistake','cpm','status','accuracy'];
+}
