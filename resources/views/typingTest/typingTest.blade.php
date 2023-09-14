d6d7b947a7bc9376a0060cffcc534bf47588c3df
Christian Lapitan <lapitanchristian258@gmail.com>
2023-09-15 04:27:08 +0800

    git error

--- resources/views/typingTest/typingTest.blade.php
+++ resources/views/typingTest/typingTest.blade.php
@@ -45,13 +45,6 @@
                         <input type="hidden" name="wpm" id="wpm1" >
                         <input type="hidden" name="cpm" id="cpm1" >
                         <input type="hidden" name="inpm" id="chpm">
-<<<<<<< HEAD
-                        <button id="_retake" onclick="retake()" style="display: none;">Retake</button>
-                    </div>
-                </div>
-            </div>  
-            {{-- <div id="submitContainer" style="display: none;">
-=======
                         <button id="_refresh" class="_refresh" style="display: none;">
                             Submit
                         </button>
@@ -63,7 +56,6 @@
                 <p class="mb-4">
                     Click here to submit
                 </p>
->>>>>>>>> Temporary merge branch 2
                 <button id="myButton" >Done</button>
             </div> 
         </form>
